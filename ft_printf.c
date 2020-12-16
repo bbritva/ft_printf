04e6a03e167f7da91d53a8bdf9174bfdb3a5789b
grvelva <grvelva@student.21-school.ru>
2020-12-16 19:55:14 +0300

    добавил защиту маллока

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: grvelva <grvelva@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 18:26:12 by grvelva           #+#    #+#             */
-/*   Updated: 2020/12/16 18:49:06 by grvelva          ###   ########.fr       */
+/*   Updated: 2020/12/16 19:48:14 by grvelva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int		ft_printf(const char *str, ...)
 {
 	va_list		args;
-	s_output	*frmt;
+	t_frmt		*frmt;
 	int			result;
 
 	result = 0;
@@ -23,19 +23,19 @@ int		ft_printf(const char *str, ...)
 	while (*str)
 	{
 		if (*str != '%')
-		{
-			ft_putchar_fd(*str++, 1);
-			result++;
-		}
+			result += write(1, str++, 1);
 		else
-		{
-			frmt = frmt_parser(str, args);
-			str += frmt->parsed_size;
-			result += frmt_print(args, frmt);
-			if (frmt)
+			if ((frmt = frmt_parser(str, args)))
+			{
+				str += frmt->parsed_size;
+				result += frmt_print(args, frmt);
 				free(frmt);
-			frmt = NULL;
-		}
+			}
+			else
+			{
+				result = -1;
+				break;
+			}
 	}
 	va_end(args);
 	return (result);
